02bf090946ffcc72921808be172192db37822aa8
Alex Ghattas <ghattas.alex@gmail.com>
2017-03-18 18:12:07 -0400

    finished editing function on individual posts

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -36,7 +36,6 @@ function Post(props) {
 	return (
 		<li>
 			<i className="fa fa-trash" onClick={() => props.removePost(props.data.key)}></i>
-			<i className="fa fa-pencil" ></i>
 			<h1>{props.data.title}</h1>
 			<p>{props.data.content}</p>
 		</li>
@@ -151,8 +150,10 @@ class SinglePost extends React.Component {
 	constructor() {
 			super()
 			this.state = {
+				editing: false,
 				post: {}
-			}
+			};
+			this.save = this.save.bind(this);
 		}
 		componentDidMount() {
 			firebase.database().ref().on('value', (res) => {
@@ -161,13 +162,49 @@ class SinglePost extends React.Component {
 				this.setState({
 					post: userData[userPostKey]
 				})
+				this.setState({})
 			});
 		}
+	save(e) {
+		e.preventDefault();
+		
+		const dbRef = firebase.database().ref(this.props.params.blog_key)
+
+		dbRef.update({
+			title: this.postTitle.value,
+			content: this.postContent.value
+		})
+
+		this.setState({
+			editing: false
+		})
+	}
 	render() {
-		return (
+		let editingTemp = (
 			<div>
 				<h1>{this.state.post.title}</h1>
 				<p>{this.state.post.content}</p>
+			</div>
+		)
+
+		if (this.state.editing) {
+			editingTemp = (
+				<form onSubmit={this.save}>
+					<div>
+						<input type="text" defaultValue={this.state.post.title} name='title' ref={ref => this.postTitle = ref}/>
+					</div>
+					<div>
+						<input type="text" defaultValue={this.state.post.content} name='content' ref={ref => this.postContent = ref}/>
+					</div>
+					<input type="submit" value="Done Editing"/>
+				</form>
+			)
+		}
+
+		return (
+			<div>
+				<i className="fa fa-pencil" onClick={() => this.setState({editing: true})}></i>
+				{editingTemp}
 			</div>
 		)
 	}
